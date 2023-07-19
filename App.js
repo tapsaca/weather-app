74df870cacbfef68705c30c8b7127ddfc6ddea63
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-07-19 23:18:37 +0300

    Add navigation with bottom tabs

--- App.js
+++ App.js
@@ -1,22 +1,63 @@
-import { StyleSheet, View } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
 import City from './src/screens/City'
 import CurrentWeather from './src/screens/CurrentWeather'
 import UpcomingWeather from './src/screens/UpcomingWeather'
 
-const App = () => {
-  const { container } = styles
+const Tab = createBottomTabNavigator()
 
+const App = () => {
   return (
-    <View style={container}>
-      <CurrentWeather />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'white',
+          tabBarInactiveTintColor: 'gray'
+        }}
+      >
+        <Tab.Screen
+          component={CurrentWeather}
+          name={'Current'}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                color={focused ? 'white' : 'black'}
+                name={'droplet'}
+                size={25} 
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          component={UpcomingWeather}
+          name={'Upcoming'}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                color={focused ? 'white' : 'black'}
+                name={'droplet'}
+                size={25} 
+              />
+            )
+          }}
+        />
+        <Tab.Screen
+          component={City}
+          name={'City'}
+          options={{
+            tabBarIcon: ({ focused }) => (
+              <Feather
+                color={focused ? 'white' : 'black'}
+                name={'home'}
+                size={25}
+              />
+            )
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  }
-})
 
 export default App
