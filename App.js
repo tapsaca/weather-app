1dc0c549a6939d9400cda647109c12a17a323fd7
tapsaca <98756983+tapsaca@users.noreply.github.com>
2023-07-22 23:03:30 +0300

    Add expo location and ActivityIndicator to App

--- App.js
+++ App.js
@@ -1,12 +1,51 @@
 import { NavigationContainer } from '@react-navigation/native'
+import * as Location from 'expo-location'
+import { useEffect, useState } from 'react'
+import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import Tabs from './src/components/Tabs'
 
 const App = () => {
+  const { container } = styles
+  const [error, setError] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [location, setLocation] = useState(null)
+
+  useEffect(() => {
+    (async() => {
+      let { status } = await Location.requestForegroundPermissionsAsync()
+      if (status !== 'granted') {
+        setError('Permission to access location was denied.')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync()
+      setLocation(location)
+    })()
+  }, [])
+
+  if (location) {
+    console.log(location)
+  }
+
+  if (loading) {
+    return (
+      <View style={container}>
+        <ActivityIndicator color={'blue'} size={'large'} />
+      </View>
+    )
+  }
+
   return (
     <NavigationContainer>
       <Tabs />
     </NavigationContainer>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center'
+  }
+})
 
 export default App
